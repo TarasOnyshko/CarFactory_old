d8142a60ccce0731e9f80448fdccfcead102099c
TarasOnyshko <tasik7231@gmail.com>
2021-03-11 16:56:41 +0200

    test commit

--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ namespace CarFactory
         [STAThread]
         static void Main()
         {
+            //test 1
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
